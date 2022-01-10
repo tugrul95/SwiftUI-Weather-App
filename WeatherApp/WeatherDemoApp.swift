e47192b32902d4f4a090142699b2465f44116d33
tugrul95 <tugrul1995@gmail.com>
2022-01-10 12:32:41 +0300

    final version

---
+++ WeatherApp/WeatherDemoApp.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Macbook on 04/01/22.
+//
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
